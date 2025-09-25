f8065ecf2c3bddfaadb86d0cfe019c4559e2693c
wo_oya <rkw37@naver.com>
2025-09-25 12:55:35 +0900

    fetch /api/stock/main

--- script.js
+++ script.js
@@ -171,7 +171,7 @@ document.addEventListener("DOMContentLoaded", () => {
   loadChartDataFromResponse('oil-widget', 'data/oil.json', 'ochat'); // 실제 키 확인 필요
 
   // 백엔드 API 호출
-  fetch("http://localhost:4002/stock/main")
+  fetch("http://api/stock/main")
     .then((response) => response.json())
     .then((data) => {
       if (data.code === "SU") {
